b2c8812b8d384739354d69968632dd5f8218296d
mazen <mazenhaytham26@gmail.com>
2026-01-31 06:18:13 +0200

    ALL DB MODELS ARE ADDED

---
+++ src/app.ts
@@ -0,0 +1,27 @@
+import dotenv from "dotenv";
+import express from "express";
+import type { Request, Response } from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import { prisma } from "./shared/prisma.ts";
+
+dotenv.config();
+
+const app = express();
+
+app.use(helmet());
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.get("/api/users", (req: Request, res: Response) => {
+  return res.status(200).json({
+    data: [
+      {
+        id: 1,
+        name: "Mazen",
+      },
+    ],
+  });
+});
+
+export default app;
