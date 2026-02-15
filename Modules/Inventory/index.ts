0b1c0eb707e32198b514a941ab34e4bdbc3d9c2b
mazen <mazenhaytham26@gmail.com>
2026-02-15 03:48:31 +0200

    added all authentication + authorization in users+inventory module

--- Modules/Inventory/index.ts
+++ Modules/Inventory/index.ts
@@ -3,18 +3,20 @@ import { InventoryRepoFactory } from "./Components/RepoFactory.js";
 import { InventoryService } from "./service/inventoryService.js";
 import { InventoryController } from "./controller/inventoryController.js";
 import { InventoryApiImp } from "./Api/InvApiImp.js";
+import { authenticate } from "../Auth/middleware/authMiddleWare.js";
+import { authorize } from "../Auth/middleware/authorizationMiddleWare.js";
 const repo = InventoryRepoFactory.getInventoryRepo("prisma");
 const inventoryService = new InventoryService(repo);
 const InventoryApi = new InventoryApiImp(inventoryService);
 const inventoryController = new InventoryController(inventoryService);
 const router = Router();
 
-router.get("/", inventoryController.getAllInventories);
-router.get("/:id", inventoryController.findInventoryById);
-router.delete("/:id", inventoryController.deactivateInventory);
-router.post("/", inventoryController.addInventory);
-router.patch("/:id", inventoryController.updateInventory);
-router.patch("/stock/:id", inventoryController.updateStockLevel);
+router.get("/", authenticate,authorize(["ADMIN"]),inventoryController.getAllInventories);
+router.get("/:id", authenticate,authorize(["ADMIN"]),inventoryController.findInventoryById);
+router.delete("/:id", authenticate,authorize(["ADMIN"]), inventoryController.deactivateInventory);
+router.post("/", authenticate,authorize(["ADMIN"]),inventoryController.addInventory);
+router.patch("/:id", authenticate,authorize(["ADMIN"]),inventoryController.updateInventory);
+router.patch("/stock/:id", authenticate,authorize(["ADMIN","SUPPLIER"]),inventoryController.updateStockLevel);
 
 export { router as inventoryRouter };
 export { InventoryApi };
